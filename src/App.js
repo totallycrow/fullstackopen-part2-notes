31e9d16a935e09aeaa417f8c93a2d579d38eb797
Adrian Gajda <adrian.gajda@hotmail.com>
2022-05-31 20:02:44 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import Note from './components/Note'
+import { useState } from "react";
+import Note from "./components/Note";
 
-const App = ({ notes } ) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+  };
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
+        {notes.map((note) => (
           <Note key={note.id} note={note} />
-        )}
+        ))}
       </ul>
-    </div>
-  )
-}
 
-export default App
+      <form onSubmit={addNote}>
+        <input />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
